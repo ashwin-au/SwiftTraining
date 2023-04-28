44bc1723dd2466b1c3baee98315ceee93c47d928
Ashwin <v-asunnikrishna@expediagroup.com>
2023-04-28 09:08:56 +0530

    cardView

--- TrainingSwift/TrainingSwift/Extension/Color+/Color+.swift
+++ TrainingSwift/TrainingSwift/Extension/Color+/Color+.swift
@@ -10,4 +10,10 @@ import SwiftUI
 extension Color {
     static let primaryFontColor = Color("primaryFontColor")
     static let secondaryFontColor = Color("secondaryFontColor")
+    static let secondaryTextColor = Color("secondaryTextColor")
+    static let primaryTextColor = Color("primaryTextColor")
+    static let secondaryBgColor = Color("secondaryBgColor")
+    static let buttonColor = Color("buttonColor")
+    
 }
+
--- TrainingSwift/TrainingSwift/Networking/APIManager.swift
+++ TrainingSwift/TrainingSwift/Networking/APIManager.swift
@@ -10,17 +10,19 @@ import Foundation
 class APIManager {
     static let shared = APIManager()
     private init() { }
-   
-    func fetch<T: Codable>(with url: String, type: T.Type) async  -> (Result<T, NetworkError>) {
+}
+
+extension APIManager: APIHelperProtocol {
+    func fetch<T>(with url: String, type: T.Type) async -> (Result<T, NetworkError>) where T: Decodable, T: Encodable {
         guard let url = URL(string: url) else { return .failure(.badURL) }
         let urlRequest = URLRequest(url: url)
         do {
             let (data, _) = try await URLSession.shared.data(for: urlRequest)
             let responseData = await formatDataModel(data: data, type: T.self)
             switch responseData {
-            case .success(let dataModel):
+            case let .success(dataModel):
                 return .success(dataModel)
-            case .failure(let error):
+            case let .failure(error):
                 return .failure(error)
             }
         } catch {
@@ -28,12 +30,23 @@ class APIManager {
             return .failure(.networkError)
         }
     }
-    
-    func formatDataModel<T: Codable>(data: Data, type: T.Type) async -> (Result<T, NetworkError>) {
+
+    func formatDataModel<T>(data: Data, type: T.Type) async -> (Result<T, NetworkError>) where T: Decodable, T: Encodable {
         if let responseModel = try? JSONDecoder().decode(T.self, from: data) {
             return .success(responseModel)
-        } else  {
+        } else {
             return .failure(.parseError)
         }
     }
+}
+
+protocol APIFetchProtocol {
+    func fetch<T: Codable>(with url: String, type: T.Type) async -> (Result<T, NetworkError>)
+}
+
+protocol JsonHandlerProtocol {
+    func formatDataModel<T: Codable>(data: Data, type: T.Type) async -> (Result<T, NetworkError>)
+}
+
+protocol APIHelperProtocol: APIFetchProtocol, JsonHandlerProtocol {
 }
